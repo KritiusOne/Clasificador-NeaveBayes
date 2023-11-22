b5075a80422558eb2321199017b859c3f20db520
Julio Rodríguez <julio10489@gmail.com>
2023-11-22 02:46:03 -0500

    Separacion de los unicos y entrenamiento de su propio modelo

--- main.py
+++ main.py
@@ -8,34 +8,44 @@ from Types import alphas
 
 
 df = getDataSet()
-print(df["screen_name"].head(10))
-train_data, test_data = train_test_split(df, test_size=0.2, random_state=42)
+df_unique = df.groupby("screen_name").agg(
+    {"screen_name": "first", "text": "first"})
+df_purgate = df[df["screen_name"].duplicated()]
 
-alpha_accuracies = defaultdict(list)
 
+def getClasificacion(df):
+    train_data, test_data = train_test_split(
+        df, test_size=0.2, random_state=42)
 
-for alpha in alphas:
+    alpha_accuracies = defaultdict(list)
+
+    for alpha in alphas:
+        prior_probs = train_data['screen_name'].value_counts(
+            normalize=True).to_dict()
+        accuracy, _ = train_and_evaluate(
+            prior_probs, train_data, test_data, alpha)
+        alpha_accuracies[alpha].append(accuracy)
+
+    mean_accuracies = {alpha: np.mean(acc)
+                       for alpha, acc in alpha_accuracies.items()}
+    best_alpha = max(mean_accuracies, key=mean_accuracies.get)
+    best_accuracy = mean_accuracies[best_alpha]
+
     prior_probs = train_data['screen_name'].value_counts(
         normalize=True).to_dict()
-    accuracy, _ = train_and_evaluate(prior_probs, train_data, test_data, alpha)
-    alpha_accuracies[alpha].append(accuracy)
+    accuracy, summary_table = train_and_evaluate(
+        prior_probs, train_data, test_data, best_alpha)
+    # esto de aca es para que el pandas no resuma la tabla sino que la muestre toda no es tan importante
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
+    accuracy = accuracy*100
+    print("\nSummary Table:")
+    print(summary_table)
+
+    print(f"\nPrecisión : {accuracy:.8f}%")
 
 
-mean_accuracies = {alpha: np.mean(acc)
-                   for alpha, acc in alpha_accuracies.items()}
-best_alpha = max(mean_accuracies, key=mean_accuracies.get)
-best_accuracy = mean_accuracies[best_alpha]
-
-prior_probs = train_data['screen_name'].value_counts(normalize=True).to_dict()
-accuracy, summary_table = train_and_evaluate(
-    prior_probs, train_data, test_data, best_alpha)
-# esto de aca es para que el pandas no resuma la tabla sino que la muestre toda no es tan importante
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
-accuracy = accuracy*100
-print("\nSummary Table:")
-print(summary_table)
-
-print(f"\nPrecisión : {accuracy:.8f}%")
+getClasificacion(df_unique)
+getClasificacion(df_purgate)
